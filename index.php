bf906934888fef52853de04ee95caf70b767e82a
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2024-03-12 10:24:16 +0700

    Integrate DataTables Bootstrap 5

--- index.php
+++ index.php
@@ -6,31 +6,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="styles\style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.2/css/dataTables.bootstrap5.css">
+
+    <script defer src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+    <script defer src="https://cdn.datatables.net/2.0.2/js/dataTables.js"></script>
+    <script defer src="https://cdn.datatables.net/2.0.2/js/dataTables.bootstrap5.js"></script>
+    <script defer src="styles\script.js"></script>
 </head>
 
 <body>
     <?php include("connection.php") ?>
     <header style="background-color:goldenrod; display: block">
-        <h1 align="center" style="margin: 2px;">Toko Roti Oemar Bakery</h1>
-        <p align="center" style="margin: 1px;">Nikmatnya mendunia</p>
+        <h1 class="display-1" align="center" style="margin: 2px;">Toko Roti Oemar Bakery</h1>
+        <p class="display-6" align="center" style="margin: 1px;">Nikmatnya mendoenia</p>
         <br>
     </header>
-    <div style="text-align: center; background-color:bisque">
+    <div class="container-fluid" style="text-align: center; background-color:azure">
     <br>
-        <table border="1" style="margin: auto">
+        <table border="1" style="margin: auto" class="table table-striped" id="tabel">
             <thead>
                 <tr>
                     <th>Gambar</th>
                     <th>Deskripsi</th>
                 </tr>
             </thead>
+            <tbody>
             <?php
             $table = $conn->query("SELECT * FROM roti");
             while ($row = $table->fetch_assoc()) {
                 extract($row);
             ?>
                 <tr>
-                    <td><img src="assets\images\<?=$foto?>" alt="roti-sesal" width="150px"></td>
+                    <td><img src="assets\images\<?=$foto?>" alt="roti-sesal" width="150px" class="rounded mx-auto d-block"></td>
                     <td>
                         <ul style="list-style-type: none; padding-left: 2px; margin-left: 10px; margin-right: 15px">
                             <li><?=$nama_roti?></li>
@@ -43,6 +52,7 @@
             <?php
             }
             ?>
+            </tbody>
         </table>
     </div>
 </body>

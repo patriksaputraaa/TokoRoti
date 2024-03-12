8096df29e3720e410ea6f0e8ae3372719ba3ce16
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2024-03-12 12:39:25 +0700

    Add to DB and Upload Item

--- save.php
+++ save.php
@@ -1,6 +1,22 @@
 <?php
-extract($_POST);
-echo "$nama_roti,$diameter,$warna,$harga";
-$file = $_FILES['foto'];
-$fileName = $file['name'];
+include("connection.php");
+
+if($_POST){
+    $LOCATION = "assets/images/";
+
+    extract($_POST);
+    $file = $_FILES['foto'];
+    $fileName = $file['name'];
+    $fileTempPath = $file['tmp_name'];
+    $fileFormat = pathinfo($fileName, PATHINFO_EXTENSION);
+
+    if(is_uploaded_file($fileTempPath)){
+        $newFileName = strtolower(str_replace(' ', '', $nama_roti)) . '.' . $fileFormat;
+        move_uploaded_file($fileTempPath, $LOCATION.$newFileName);
+        $sql = "INSERT INTO roti (nama_roti, diameter, warna, foto, harga) VALUES ('$nama_roti', '$diameter', '$warna', '$newFileName', '$harga')";
+        $conn->query($sql);
+        $conn->close();
+    }
+    header("Location: index.php");
+}
 ?>
